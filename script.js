3e8cc62e1f3a85f0a935107b6d6933e6aaf903bb
Michael Larkins <mlarkinsii@gmail.com>
2015-11-20 14:57:53 -0500

    assessment wk 4

---
+++ script.js
@@ -0,0 +1,20 @@
+(function(){
+	var animals = [
+		{name: 'cow', sound: 'moo'},
+		{name: 'sheep', sound: 'baaah'},
+		{name: 'dog', sound: 'arf'},	
+	}
+
+
+function seeAndSay(farm) {
+	console.log(animal.name, animal.sound);
+}
+animals.forEach(seeAndSay){
+	animals.color = gray;
+])();
+
+
+
+
+
+console.log('the ' + animals.name + 'says ' + animals.sound + '!' + ' And is '
